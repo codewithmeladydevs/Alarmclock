424db0e9c7470c9341c28629dd8f5450e93fafb7
Jennifer Pollack <jenniferpollack@apcmc2023.local>
2021-04-24 14:21:41 +0200

    Applied threading and added a stop button

--- vanessa_alarm_clock.py
+++ vanessa_alarm_clock.py
@@ -6,8 +6,11 @@ import tkinter as tk
 from datetime import datetime, date
 import time
 import pygame
+from threading import *
 
 print("Hello World. Time to Create an Alarm Clock.")
+
+running = True # Global Flag
 
 def set_time():
 #    string = time.strftime('%H:%M:%S %p')
@@ -15,20 +18,39 @@ def set_time():
     current_time = get_current_time.strftime("%H:%M:%S %p")
     lbl.config(text = current_time)
     lbl.after(1000, set_time)
+    return 
+
+def threading():
+    # call set_alarm_clock
+    t1=Thread(target=set_alarm_clock)
+    t1.start()
 
 def set_alarm_clock():
+    global running 
     alarm_time_input = f"{hour.get()}:{min.get()}:{sec.get()}"
 
     while True:
         now = datetime.now().strftime("%H:%M:%S")
-
-        pygame.mixer.init()
+        
         if now == alarm_time_input:
-            print("Wake Up!")
-            pygame.mixer.music.load("alarm-clock-01.wav")
-            pygame.mixer.music.play()
-            time.sleep(5)
+            play_alarm()
             break
+
+def play_alarm():
+    global running 
+
+    print("Wake Up!")
+    pygame.mixer.init()
+    pygame.mixer.music.load("alarm-clock-01.wav")
+    pygame.mixer.music.play()
+    time.sleep(5)
+    
+
+def stop_alarm_clock():
+    global running
+    running = False
+    pygame.mixer.music.stop()
+
 
 clock = tk.Tk()
 clock.title("Vanessa's Alarm Clock")
@@ -36,8 +58,8 @@ clock.geometry("400x200")
 lbl = tk.Label(clock, font = ('calibri', 40, 'bold'),
     background = 'purple',
     foreground = 'white')
-
 lbl.pack(anchor = 'center', ipadx=30, padx=30)
+
 set_time()
 
 setYourAlarm = tk.Label(clock,text = "Set time to wake you up",fg="blue",relief = "solid",font=("Helevetica",10,"bold")).place(x=0, y=70)
@@ -48,12 +70,12 @@ min = tk.StringVar()
 sec = tk.StringVar()
 
 #Time required to set the alarm clock:
-hourTime = tk.Entry(clock, textvariable = hour, bg = "pink", width = 15).place(x=110,y=100)
-minTime  = tk.Entry(clock, textvariable = min, bg = "pink", width = 15).place(x=150,y=100)
-secTime  = tk.Entry(clock, textvariable = sec, bg = "pink", width = 15).place(x=200,y=100)
+hourTime = tk.Entry(clock, textvariable = hour, bg="pink", width=15).place(x=100,y=100)
+minTime  = tk.Entry(clock, textvariable = min, bg="pink", width=15).place(x=150,y=100)
+secTime  = tk.Entry(clock, textvariable = sec, bg="pink", width=15).place(x=200,y=100)
 
 #To take the time input by user:
-submit = tk.Button(clock,text = "Set Alarm",fg="red",width = 10,command = set_alarm_clock).place(x =110,y=140)
-
+submit = tk.Button(clock,text = "Set Alarm",fg="red",width = 10,command=threading).place(x =100,y=140)
+stop = tk.Button(clock, text = "Stop Alarm",fg="red",width=10, command=stop_alarm_clock).place(x=240,y=140)
 
 clock.mainloop()
